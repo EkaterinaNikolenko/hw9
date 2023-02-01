3b3e4bd1e5e31e667c89d81cb2354067859d7834
Nikolenko Front End Pro 24.01.23 <nikolenkoekaterina@gmail.com>
2023-02-01 18:51:34 +0200

    mult table

--- index.js
+++ index.js
@@ -64,8 +64,17 @@ console.log(countDel);
 console.log(sumDel);
 
 for(let i = 1; i <= 10; i++){
+    let arr = [];
+    for(let j = 1; j <= 10; j++){
+        arr.push(i * j);
+    }
+    console.log(arr.join('\t'));
+}
+
+for(let i = 1; i <= 10; i++){
     console.log('');
     for(let j = 1; j <= 10; j++){
         console.log(`${i} * ${j} = ${i * j}`);
     }
 }
+
